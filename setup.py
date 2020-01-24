2f0ee33b269192b03a4294af63367f00a362b87f
Ravsehaj singh puri <ravsehaj@icloud.com>
2020-01-24 12:22:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="topsis_by_ravsehaj", # Replace with your own username
-    version="0.0.4",
+    name="topsisbyravsehaj", # Replace with your own username
+    version="0.0.5",
     author="Ravsehaj Singh Puri",
     author_email="ravsehajsinghpuri@gmail.com",
     description="A package for Topsis technique",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Ravsehajsinghpuri/topsis_by_ravsehaj",
+    url="https://github.com/Ravsehajsinghpuri/topsisbyravsehaj",
     packages=setuptools.find_packages(),
     install_requires=[
        'numpy','pandas','scipy',
